fff806f95c27fbb4e206a37e2924b9357da4decf
Март <158443297+sitorishinee@users.noreply.github.com>
2024-08-04 20:01:46 +0300

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,26 +2,25 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {
-  return (
-    <header className="header">
-      <h1>Lumirum - аниме онлайн</h1>
-      <div class="sections">
-        <a href="/ongoing">Онгоинги</a>
-        <a href="/latest">Новые серии</a>
-        <a href="/anons">Анонсы</a>
-        <a href="/finished">Завершенные</a>
-        <div class="dropdown">
-          <a href="#">Категории <i class="fa fa-angle-down"></i></a>
-          <ul class="category">
-            <li><a href="/popular">Популярное</a></li>
-            <li><a href="/timesheet">Расписание</a></li>
-            <li><a href="/collections">Коллекции</a></li>
-            <li><a href="/release/random">Случайное аниме</a></li>
-          </ul>
-        </div>
-      </div>
-    </header>
-  );
-};
+ return (
+   <header>
+     <div className="sections">
+       <a href="/ongoing">Онгоинги</a>
+       <a href="/latest">Новые серии</a>
+       <a href="/anons">Анонсы</a>
+       <a href="/finished">Завершенные</a>
+       <div className="dropdown">
+         <a href="#">Категории <i className="fa fa-angle-down"></i></a>
+         <ul className="category">
+           <li><a href="/popular">Популярное</a></li>
+           <li><a href="/timesheet">Расписание</a></li>
+           <li><a href="/collections">Коллекции</a></li>
+           <li><a href="/release/random">Случайное аниме</a></li>
+         </ul>
+       </div>
+     </div>
+   </header>
+ );
+}
 
 export default Header;
