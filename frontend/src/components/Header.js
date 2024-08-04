10198de8acce9c9193b528a2aecb889b6e5ab0fd
Март <158443297+sitorishinee@users.noreply.github.com>
2024-08-04 14:33:29 +0300

    Create Header.js

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'; // Если у тебя будут стили
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Lumirum - аниме онлайн</h1>
+    </header>
+  );
+};
+
+export default Header;
