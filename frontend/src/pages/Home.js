72c4878e9f8190ede4f42477e36f36c84b4d15e7
Март <158443297+sitorishinee@users.noreply.github.com>
2024-08-04 14:35:27 +0300

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <p>Добро пожаловать на сайт по аниме</p>
+    </div>
+  );
+};
+
+export default Home;
